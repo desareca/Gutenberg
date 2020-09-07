283008af96184ad1f7b256be215419d930b4a546
desareca <carlos.saquel@gmail.com>
2020-09-06 22:43:26 -0300

    primer commit

---
+++ README.md
@@ -0,0 +1,32 @@
+## Propósito
+
+Este repositorio está enfocado en procesamiento y análisis de textos, para ello se utilizan los libros que se encuentran en la página [www.gutenberg.org](www.gutenberg.org).
+
+
+<br>
+
+![](pg-logo-129x80.png)
+
+El **Proyecto Gutenberg (PG)** fue desarrollado por [Michael Hart](https://es.wikipedia.org/wiki/Michael_Hart_(fil%C3%A1ntropo))  en 1971 con el fin de crear una biblioteca de libros electrónicos gratuitos a partir de libros que ya existen físicamente. Estos libros electrónicos se encuentran disponibles desde entonces en Internet. Son gratuitos. Los libros pueden ser leídos en diferentes formatos como Kindle, EPUB, incluso ASCII, UTF-8 o leerlos en línea en formato HTML.
+
+
+### Descripción
+
+Se trata de la biblioteca digital más antigua, pues su fundación data de 1971. El proyecto Gutenberg se centra en la literatura históricamente importante y obras de referencia. El lema del proyecto es «*cortemos los barrotes de la ignorancia y el analfabetismo*», escogido porque el proyecto espera continuar con el trabajo de la difusión de la alfabetización pública y de la gratitud hacia nuestra herencia literaria que las bibliotecas públicas empezaron a principios del siglo XX. Siempre que es posible, las ediciones de Gutenberg se liberan en formato ASCII. También se liberan en otros formatos, siempre que sean presentados por voluntarios. Durante algunos años anteriores, hubo discusiones sobre si sería conveniente usar algún formato tipo XML, aunque no hubo muchos avances entonces. A los formatos populares que no son fácilmente editables, como el PDF, no se les consideraba apropiados, de acuerdo con los objetivos generales del proyecto Gutenberg, aunque se han añadido muchos a la colección. Actualmente se publican libros en varios de los formatos electrónicos para leer en dispositivos PDA y lectores de libros electrónicos, y también en formato de audiobooks.
+
+Los textos que se proporcionan son principalmente de dominio público, bien porque estos derechos ya han expirado y la obra se encuentra en dominio público, bien porque se obtuvo la autorización del autor o titular de los derechos de autor. También hay algunos textos bajo derechos de autor que el proyecto Gutenberg ha hecho disponibles con el permiso de sus escritores. Al proyecto se le puso el nombre del impresor alemán del siglo XV Johannes Gutenberg, quien inventó la imprenta de tipos móviles.
+
+Fuente [Wikipedia](https://es.wikipedia.org/wiki/Proyecto_Gutenberg)
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Web Scraping Books Spanish/Script.R
@@ -0,0 +1,226 @@
+library(rvest)
+library(tidyverse)
+library(tools)
+library(tm)
+
+# Descarga de libros en español de la página http://www.gutenberg.org
+# Seleccion de libros (N° libro) en español
+# -------------------------------------------------------------------------------------------------------------
+url <- "http://www.gutenberg.org/browse/languages/es"
+tmp <- read_html(url)
+tmp <- html_nodes(tmp, "li") %>% html_nodes("a")
+gutEsp <- tmp %>% lapply(html_attr, name="href") %>% data.frame()
+gutEsp <- gutEsp %>% select(matches("ebooks")) %>% t()
+gutEsp <-gsub("/ebooks/", "", gutEsp) %>% as.numeric(gutEsp) %>% na.omit() %>% unique()
+
+
+# Scraping a las base de datos de loslibros encontrados (698)
+# -------------------------------------------------------------------------------------------------------------
+gutenbergScrap <- function(n=c(1:10), items=NULL, verbose=TRUE, save.RDS=TRUE, titleData="Books Gutenberg"){
+   if(is.null(items)){items <- c("Author",
+                                 "Title", 
+                                 "Language", 
+                                 "LoC Class",
+                                 "Subject", 
+                                 "Category", 
+                                 "EBook-No.",
+                                 "Release Date")}
+   titleData <- paste0(titleData, ".rds")
+   G <- data.frame()
+   if(verbose) {pb <- txtProgressBar(min = 0, max = length(n), style = 3)}
+   j <- 1
+   for(i in n){
+      url <- paste0("http://www.gutenberg.org/ebooks/", i)
+      tmp <- tryCatch(read_html(url), error = function(e) "error")
+      if(tmp[1]!="error"){
+         tmp <- html_nodes(tmp, "table")
+         gut <- html_table(tmp[[2]]) %>% filter(X1 %in% items)
+         gut2 <- data.frame(X1 = items, X2 = NA)
+         for(k in c(1:length(items))){
+            if(dim(gut[gut$X1==items[k],])[1]<1){gut2[k,2] <- ""}
+            if(dim(gut[gut$X1==items[k],])[1]>=1){gut2[k,2] <- (gut %>% filter(X1==items[k]))[,2] %>% paste(collapse = " -- ")}
+         }
+         
+         id <- which(html_table(tmp[[1]])[,2]=="Plain Text UTF-8")
+         if(length(id)>0){g <- html_table(tmp[[1]])[id,3][1]}
+         if(length(id)==0){g <- ""}
+         gut2 <- rbind(gut2, g)
+         
+         if(i==1){G <- t(gut2)}
+         if(i>1){G <- rbind(G, gut2 %>% select(2) %>% t())}
+         
+      }
+      if(verbose){setTxtProgressBar(pb, j)}
+      j <- j + 1
+   }
+   colnames(G) <- c(items, "url")
+   G <- (G %>% as.data.frame())
+   rownames(G) <- c(1:dim(G)[1])
+   if(verbose){close(pb)}
+   if(save.RDS){saveRDS(G,titleData)}
+   return(G)
+}
+t1 = Sys.time()
+G <- gutenbergScrap(n=gutEsp, titleData="Books Gutenberg Spanish")
+print(difftime(Sys.time(), t1, units = 'min')) # toma alrededor de 8 mins en 698 libros
+
+
+# descargar archivos
+# -------------------------------------------------------------------------------------------------------------
+gutenbergDown <- function(url, path = "books/"){
+   books <- gsub("https://www.gutenberg.org/ebooks/","",url$url)
+   books <- gsub("https://www.gutenberg.org/files/\\d{1,10}/","",books)
+   books <- gsub(".utf-8","",books)
+   if(!dir.exists(path)){dir.create(path)}
+   
+   c(1:length(books)) %>% 
+      sapply(function(x){
+         tryCatch(download.file(url = url$url[x], destfile = paste0(path, books[x])),
+                  error = function(e) "error")
+      })
+   return("Descarga finalizada!")
+}
+G <- readRDS("Books Gutenberg Spanish.rds")
+t1 = Sys.time()
+gutenbergDown(url = G %>% select(url), path = "books Spanish/")
+print(difftime(Sys.time(), t1, units = 'min')) # toma alrededor de 54 mins en 688 libros
+
+
+
+# carga de libros
+# -------------------------------------------------------------------------------------------------------------
+# Seleccionar los numeros de los ebooks descargados
+listFiles <- list.files(path = "books Spanish")
+listFiles <- gsub("-0.txt", ".txt", listFiles)
+listFiles <- gsub(".txt", "", listFiles) %>% as.numeric()
+
+# filtrar los ebooks descargados y sólo en español
+G <- readRDS("Books Gutenberg Spanish.rds")
+G <- G %>% filter(`EBook-No.` %in% listFiles) %>% filter(Language=="Spanish")
+
+# nombre se los archivos solo en español
+books <- gsub("https://www.gutenberg.org/ebooks/","",G$url)
+books <- gsub("https://www.gutenberg.org/files/\\d{1,10}/","",books)
+books <- gsub(".utf-8","",books)
+
+# carga de libros descargados en español
+books <- books[-602] #problemas al leer el corpus
+G <- G[-602,] #problemas al leer el corpus
+
+# creación corpus
+corp <- VCorpus(VectorSource(c(1:length(books)) %>% 
+                                sapply(function(x){read_file(file = paste0("books Spanish/", books[x]))})),
+                readerControl = list(language = "es"))
+
+
+# crea el transformador de contenido de toSpace
+toSpace <- content_transformer(function(x, pattern, y=F) {return(gsub(pattern, " ", x, perl = y))})
+# dominios web de paises
+# source("domainWeb.R")
+
+# limpieza (se dejan las stopwords en esta etapa)
+t1 = Sys.time()
+corp <- corp %>% 
+   # tm_map(removeWords, words = domWeb) %>% 
+   tm_map(toSpace, pattern = c("[[:punct:]]"), y = TRUE) %>% 
+   tm_map(toSpace, pattern = c("¿")) %>% 
+   tm_map(toSpace, pattern = c("¡")) %>% 
+   tm_map(removeNumbers) %>% 
+   tm_map(content_transformer(tolower)) %>% 
+   tm_map(removeWords, words = c(letters[-c(1,15,21,25)], "\\n", "\\r")) %>% 
+   tm_map(content_transformer(str_replace_all), pattern = "\\p{quotation mark}", replacement = " ") %>% 
+   # tm_map(removeWords, stopwords("spanish")) %>% 
+   tm_map(removeWords, words = c(" $", "^ ")) %>% 
+   tm_map(stripWhitespace)
+print(difftime(Sys.time(), t1, units = 'min')) # toma alrededor 2 mins 20 libros
+# aprox deberia tomar 60 mins 662 libros// 66 min final
+
+pathClean <- "Clean Books Spanish/"
+if(!dir.exists(pathClean)) dir.create(path = pathClean)
+
+c(1:length(books)) %>% sapply(function(x){
+   corp[[x]]["content"] %>% unlist() %>% write(file = paste0(pathClean, books[x]))
+})
+
+
+
+# carga de libros limpios
+# -------------------------------------------------------------------------------------------------------------
+# Seleccionar los numeros de los ebooks descargados
+listFiles <- list.files(path = "books Spanish")
+listFiles <- gsub("-0.txt", ".txt", listFiles)
+listFiles <- gsub(".txt", "", listFiles) %>% as.numeric()
+
+# filtrar los ebooks descargados y sólo en español
+G <- readRDS("Books Gutenberg Spanish.rds")
+G <- G %>% filter(`EBook-No.` %in% listFiles) %>% filter(Language=="Spanish")
+
+# nombre se los archivos solo en español
+books <- gsub("https://www.gutenberg.org/ebooks/","",G$url)
+books <- gsub("https://www.gutenberg.org/files/\\d{1,10}/","",books)
+books <- gsub(".utf-8","",books)
+
+# carga de libros descargados en español
+books <- books[-602] #problemas al leer el corpus
+G <- G[-602,] #problemas al leer el corpus
+
+
+# creación corpus limpio
+read_txt <- function(path){
+   con <- file(path, open="rb") # Abrimos la conexión
+   txt <- readLines(con)          # Leemos el contenido del archivo
+   close(con)
+   return(txt)
+}
+corpClean <- VCorpus(
+   VectorSource(c(1:length(books)) %>% 
+                   sapply(function(x){read_txt(path = paste0("Clean Books Spanish/", books[x]))})),
+   readerControl = list(language = "es"))
+
+
+
+
+
+# procesado de LoC class
+# -------------------------------------------------------------------------------------------------------------
+# separación de items
+locClass <- G$`LoC Class` %>% 
+   sapply(function(x){strsplit(x, split = " -- ") %>% unlist()})
+
+# diccionario
+pasteSent <- function(x){
+   t <- c()
+   for (i in 1:length(x)) {
+      j = "; "
+      if(i == 1) {j = ""}
+      t <- paste(t, x[i], sep = j)
+   }
+   return(t)
+}
+dictClass <- data.frame(symbol = c(1:length(locClass)) %>% 
+                           sapply(function(x) {
+                              t <- c()
+                              for (i in 1:length(locClass[[x]])) {
+                                 t <- c(t, (locClass[[x]][i] %>% strsplit(split = ": "))[[1]][1])
+                              }
+                              return(t)
+                           }) %>% sapply(pasteSent), 
+                        description = c(1:length(locClass)) %>% 
+                           sapply(function(x) {
+                              t <- c()
+                              for (i in 1:length(locClass[[x]])) {
+                                 t <- c(t, (locClass[[x]][i] %>% strsplit(split = ": "))[[1]][-1])
+                              }
+                              return(t)
+                           }) %>% sapply(pasteSent),
+                        stringsAsFactors = F)
+
+dictClass$symbol %>% table() %>% barplot()
+
+
+
+
+
+
+
+
